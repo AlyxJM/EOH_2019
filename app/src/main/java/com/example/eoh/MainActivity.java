d80ef28b2989afdda88fa8fdb429d1e61c0d4e08
AlyxJM <merritt9@illinois.edu>
2019-02-21 16:25:08 -0600

    Android permissions and java visualizer implementation

--- app/src/main/java/com/example/eoh/MainActivity.java
+++ app/src/main/java/com/example/eoh/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.eoh;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.chibde.visualizer.CircleBarVisualizer;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
     private Button play, stop, record;
     //private MediaRecorder myAudioRecorder;
     private String outputFile;
@@ -19,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+
+            }
+        }
+
+
         play = findViewById(R.id.play);
         stop = findViewById(R.id.stop);
         record = findViewById(R.id.record);
@@ -80,5 +110,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d("hi","ran");
+                    CircleBarVisualizer circleBarVisualizer = findViewById(R.id.circleBarVisualizer);
+                    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.thank_you_next);
+
+                    circleBarVisualizer.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+                    circleBarVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
