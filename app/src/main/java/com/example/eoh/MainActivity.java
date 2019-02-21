a06cb3906a9ad2825a5243521d9d8dde258c20a7
AlyxJM <merritt9@illinois.edu>
2019-02-21 17:41:34 -0600

    Fix UI- basic components, updated button click implementation

--- app/src/main/java/com/example/eoh/MainActivity.java
+++ app/src/main/java/com/example/eoh/MainActivity.java
@@ -5,58 +5,47 @@ import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.chibde.visualizer.CircleBarVisualizer;
 
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
 
-    private Button play, stop, record;
+    private MediaPlayer mediaPlayer;
+    private CircleBarVisualizer circleBarVisualizer;
+
+    private Button playButton;
+    private Button stopButton;
+    private Button recordButton;
+
     //private MediaRecorder myAudioRecorder;
     private String outputFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.RECORD_AUDIO)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+        playButton = findViewById(R.id.play);
+        stopButton = findViewById(R.id.stop);
+        recordButton = findViewById(R.id.record);
 
-            }
-        }
+        stopButton.setEnabled(false);
 
+        askPermission();
 
-        play = findViewById(R.id.play);
-        stop = findViewById(R.id.stop);
-        record = findViewById(R.id.record);
-        stop.setEnabled(false);
-        play.setEnabled(false);
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+        mediaPlayer = MediaPlayer.create(this, R.raw.thank_you_next);
+        circleBarVisualizer = findViewById(R.id.circleBarVisualizer);
 
         /*
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
         myAudioRecorder = new MediaRecorder();
         myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -64,7 +53,27 @@ public class MainActivity extends AppCompatActivity {
         myAudioRecorder.setOutputFile(outputFile);
         */
 
-        record.setOnClickListener(new View.OnClickListener() {
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mediaPlayer.start();
+                    Toast.makeText(getApplicationContext(), "Playing Audio",
+                            Toast.LENGTH_LONG).show();
+
+                    circleBarVisualizer.setColor(ContextCompat.getColor(MainActivity.this,
+                            R.color.colorPrimary));
+                    circleBarVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
+
+                    playButton.setEnabled(false);
+                    stopButton.setEnabled(true);
+                } catch (Exception e) {
+                    // make something
+                }
+            }
+        });
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
@@ -77,42 +86,45 @@ public class MainActivity extends AppCompatActivity {
                     // make something
                 }
                 */
-                record.setEnabled(false);
-                stop.setEnabled(true);
+                recordButton.setEnabled(false);
+                stopButton.setEnabled(true);
                 Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
             }
         });
 
-        stop.setOnClickListener(new View.OnClickListener() {
+        stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.stop();
+                Toast.makeText(getApplicationContext(), "Audio recorded successfully",
+                        Toast.LENGTH_LONG).show();
+
                 //myAudioRecorder.stop();
                 //myAudioRecorder.release();
                 //myAudioRecorder = null;
-                record.setEnabled(true);
-                stop.setEnabled(false);
-                play.setEnabled(true);
-                Toast.makeText(getApplicationContext(), "Audio Recorder successfully", Toast.LENGTH_LONG).show();
+                recordButton.setEnabled(true);
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
             }
         });
+    }
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                try {
-                    mediaPlayer.setDataSource(outputFile);
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                    Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    // make something
-                }
+    private void askPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                // Show an explanation to the user about permission
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+
             }
-        });
-
-
-
+        }
     }
 
     @Override
@@ -124,22 +136,11 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    Log.d("hi","ran");
-                    CircleBarVisualizer circleBarVisualizer = findViewById(R.id.circleBarVisualizer);
-                    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.thank_you_next);
-
-                    circleBarVisualizer.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
-                    circleBarVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
-
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    // permission denied                }
+                    return;
                 }
-                return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
         }
     }
 }
