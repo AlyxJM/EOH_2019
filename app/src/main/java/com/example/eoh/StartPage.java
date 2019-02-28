0bf4090c3274985738c97a1de34d4b2af7f3f16b
Kimi <kimiphirano@gmail.com>
2019-02-28 17:45:05 -0600

    UI started!!

---
+++ app/src/main/java/com/example/eoh/StartPage.java
@@ -0,0 +1,25 @@
+package com.example.eoh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class StartPage extends AppCompatActivity {
+    //public static final String EXTRA_MESSAGE = "com.example.eoh.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
